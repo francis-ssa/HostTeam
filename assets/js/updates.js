78c165f5d9836effe7813a0ea4ab1f67b364950c
francissteven <francisstevenalmine@gmail.com>
2025-07-21 09:43:09 +0800

    21-25

--- assets/js/updates.js
+++ assets/js/updates.js
@@ -1,14 +1,24 @@
 
 const imagePaths = [
-    'assets/img/portfolio/14-18/1.png',
-    'assets/img/portfolio/14-18/2.png',
-    'assets/img/portfolio/14-18/3.png',
-    'assets/img/portfolio/14-18/4.png',
-    'assets/img/portfolio/14-18/5.png',
-    'assets/img/portfolio/14-18/6.png',
-    'assets/img/portfolio/14-18/7.png',
-    'assets/img/portfolio/14-18/8.png',
-    'assets/img/portfolio/14-18/9.png',
+    // 'assets/img/portfolio/14-18/1.png',
+    // 'assets/img/portfolio/14-18/2.png',
+    // 'assets/img/portfolio/14-18/3.png',
+    // 'assets/img/portfolio/14-18/4.png',
+    // 'assets/img/portfolio/14-18/5.png',
+    // 'assets/img/portfolio/14-18/6.png',
+    // 'assets/img/portfolio/14-18/7.png',
+    // 'assets/img/portfolio/14-18/8.png',
+    // 'assets/img/portfolio/14-18/9.png',
+
+    'assets/img/portfolio/21-25/1.png',
+    'assets/img/portfolio/21-25/2.png',
+    'assets/img/portfolio/21-25/3.png',
+    'assets/img/portfolio/21-25/4.png',
+    'assets/img/portfolio/21-25/5.png',
+    'assets/img/portfolio/21-25/6.png',
+    'assets/img/portfolio/21-25/7.png',
+    'assets/img/portfolio/21-25/8.png',
+    'assets/img/portfolio/21-25/9.png',
 ];
 
 let currentIndex = 0;
